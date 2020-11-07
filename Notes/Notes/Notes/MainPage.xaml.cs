7b004fbff0ee9c22d073b5bdb4e1658b84f87fbd
halikja17 <71425459+halikja17@users.noreply.github.com>
2020-11-07 15:42:35 +0100

    Notes_final Android 0.7.11.2020

---
+++ Notes/Notes/Notes/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Notes.Models;
+using SQLite;
+
+namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            listView.ItemsSource = await App.Database.GetNotesAsync();
+        }
+        async void pridat(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPageEntry
+            {
+                BindingContext = new Note()
+            });
+        }
+        async void zobrazeni(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new MainPageEntry
+                {
+                    BindingContext = e.SelectedItem as Note
+                });
+            }
+        }
+    }
+}
